7f25ee22f46d85996547003937fbd21ff664bc12
ekekekkek <ek3424@columbia.edu>
2025-10-28 21:21:24 -0400

    Testing Groq

---
+++ api/test.py
@@ -0,0 +1,9 @@
+import json
+
+def handler(request):
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+        'body': json.dumps({'message': 'Python function is working!'})
+    }
+
